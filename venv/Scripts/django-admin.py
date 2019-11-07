de477b04b322f09e872c763b2af13e4be61d7f9e
Ze Rong <S10182333@connect.np.edu.sg>
2019-11-07 10:11:35 +0800

    just move venv folder to personal_portfolio

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\ze rong\desktop\year 3 sem 2 desktop\eti\rp-portfolio\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
